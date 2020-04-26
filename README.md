4d8c1b8faa88985745c87baee90b6ef4de82307e
CyrusD123 <pseudonym112304@gmai.com>
2020-04-26 16:16:46 -0400

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# lehigh-test
+A simple application made using the concepts from Lehigh University's Software Engineering Tutorial Series.
+
+The program allows the user to cast a vote for the Democratic nominee for president, and it allows them to view the results of the vote. The program utilizes GET and POST requests handled by a python backend (using Flask) and a PostgreSQL database (locally hosted).# sl-assignments
---
+++ backend.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, request, render_template
+import json
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+cursor = conn.cursor()
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    """
+    if request.method == "POST":
+        data = request.get_json()
+
+        cursor.execute("UPDATE votes SET votenum = (votenum + 1) WHERE name = '{}'".format(data))
+        
+        return json.dumps(True)
+    """  
+    return render_template('index.html')
+
+"""
+@app.route('/submitted')
+def submitThanks():
+    return render_template("submitted.html")
+
+@app.route('/results')
+def view():
+    cursor.execute("SELECT * FROM votes")
+    return render_template('results.html', result = cursor.fetchall())
+"""
+
+if __name__ == '__main__':
+    app.run()
+    cursor.close()
+    conn.close()
