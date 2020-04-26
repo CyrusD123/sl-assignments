2a4d31f01952d47a5b62158bef5494f6da0ac614
CyrusD123 <pseudonym112304@gmai.com>
2020-04-26 17:00:10 -0400

    added os.environ.get

--- backend.py
+++ backend.py
@@ -3,9 +3,7 @@ import json
 import os
 import psycopg2
 
-print("gamer time")
-# Error is somewhere here
-DATABASE_URL = os.environ['DATABASE_URL']# <
+DATABASE_URL = os.environ.get('DATABASE_URL')# <-- Error is here
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 cursor = conn.cursor()
