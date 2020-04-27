3e185139341f239e91297657446f617455222b6e
CyrusD123 <pseudonym112304@gmai.com>
2020-04-27 15:06:43 -0400

    removed comments

--- backend.py
+++ backend.py
@@ -3,7 +3,7 @@ import json
 import os
 import psycopg2
 
-DATABASE_URL = os.environ['DATABASE_URL']# < Error is here
+DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 cursor = conn.cursor()
@@ -14,17 +14,17 @@ app = Flask(__name__)
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    """
+    
     if request.method == "POST":
         data = request.get_json()
 
         cursor.execute("UPDATE votes SET votenum = (votenum + 1) WHERE name = '{}'".format(data))
         
         return json.dumps(True)
-    """  
+    
     return render_template('index.html')
 
-"""
+
 @app.route('/submitted')
 def submitThanks():
     return render_template("submitted.html")
@@ -33,7 +33,6 @@ def submitThanks():
 def view():
     cursor.execute("SELECT * FROM votes")
     return render_template('results.html', result = cursor.fetchall())
-"""
 
 if __name__ == '__main__':
     app.run()
