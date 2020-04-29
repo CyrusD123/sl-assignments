7bb35fee4572843a1756ca7e5b74863f046c3785
CyrusD123 <pseudonym112304@gmai.com>
2020-04-29 16:15:45 -0400

    made scheduler only for Mondays

--- scheduler.py
+++ scheduler.py
@@ -1,17 +1,20 @@
-import os
-import psycopg2
+import datetime
 
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+if (datetime.datetime.today().weekday() == 0):
+    import os
+    import psycopg2
 
-cursor = conn.cursor()
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-subject_arr = ["Science", "Math", "English", "Social Studies", "World Language", "PE/Health/DE", "Special Education", "Music", "Art", "Family Consumer Science", "Technology Education", "Business", "Guidance Notes", "LCTI", "Supports"]
-for subject in subject_arr:
-    cursor.execute('UPDATE assignments SET "{}" = true'.format(subject))
+    cursor = conn.cursor()
+
+    subject_arr = ["Science", "Math", "English", "Social Studies", "World Language", "PE/Health/DE", "Special Education", "Music", "Art", "Family Consumer Science", "Technology Education", "Business", "Guidance Notes", "LCTI", "Supports"]
+    for subject in subject_arr:
+        cursor.execute('UPDATE assignments SET "{}" = true'.format(subject))
 
 
-conn.commit()
+    conn.commit()
 
-cursor.close()
-conn.close()
+    cursor.close()
+    conn.close()
